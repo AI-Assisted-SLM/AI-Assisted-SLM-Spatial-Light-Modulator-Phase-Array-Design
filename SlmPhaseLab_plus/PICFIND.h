aacdeda1d88134e8ac1a39a937d36c09eca5f142
hwzxx <zxxpernal@163.com>
2026-09-03 11:38:20 +0800

    Add files via upload

---
+++ SlmPhaseLab_plus/PICFIND.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+
+#include "afxdialogex.h"
+#include <afxcmn.h> // 确保常用 MFC 控件类型可用
+#include "SlmPhaseLabDlg.h"
+
+// PICFIND 对话框
+class PICFIND : public CDialogEx
+{
+    DECLARE_DYNAMIC(PICFIND)
+
+public:
+    explicit PICFIND(CWnd* pParent = nullptr);   // 标准构造函数
+    virtual ~PICFIND() override;
+
+    // 禁用拷贝/赋值
+    PICFIND(const PICFIND&) = delete;
+    PICFIND& operator=(const PICFIND&) = delete;
+
+#ifdef AFX_DESIGN_TIME
+    enum { IDD = IDD_PICFIND };
+#endif
+
+protected:
+    virtual void DoDataExchange(CDataExchange* pDX) override;    // DDX/DDV 支持
+
+    DECLARE_MESSAGE_MAP()
+
+protected:
+    CMFCShellTreeCtrl m_shellTree;
+    CMFCShellListCtrl m_shellList;
+    
+
+public:
+    afx_msg void OnTvnSelchangedMfcshelltree1(NMHDR* pNMHDR, LRESULT* pResult);
+    afx_msg void OnLvnItemchangedMfcshelllist1(NMHDR* pNMHDR, LRESULT* pResult);
+    afx_msg void OnBnClickedButtonYesFind();
+
+    //分窗口的地址存储的变量
+    CString m_selectedPath;
+};
---
+++ SlmPhaseLab_plus/resource.h
@@ -0,0 +1,51 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 SlmPhaseLab.rc 使用
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_SLMPHASELAB_DIALOG          102
+#define IDR_MAINFRAME                   128
+#define IDD_PICFIND                     134
+#define IDC_SIZEFORSHOW                 1010
+#define IDC_WAVELENGTH                  1011
+#define IDC_PIXEL                       1012
+#define IDC_POLOGET                     1013
+#define IDC_POLOGETTIMES                1013
+#define IDC_DISTANCE                    1015
+#define IDC_ITERATION                   1016
+#define IDC_BUTTON_IN                   1017
+#define IDC_BUTTON_DOTPARK              1018
+#define IDC_BUTTON_GS                   1019
+#define IDC_BUTTON_FS                   1020
+#define IDC_BUTTON_OUTPHASE             1021
+#define IDC_BUTTON_CANCEL               1022
+#define IDC_PROGRESS2                   1025
+#define IDC_PIC_TARGET                  1027
+#define IDC_PIC_XW                      1028
+#define IDC_PIC_CJ                      1029
+#define IDC_PIC_PM                      1030
+#define IDC_BUTTON_                     1035
+#define IDC_MFCSHELLTREE1               1036
+#define IDC_MFCSHELLLIST1               1037
+#define IDC_BUTTON_YES_FIND             1038
+#define IDC_COMBO2                      1040
+#define IDC_                            1040
+#define IDC_DISTSNCE_CHOOSE             1040
+#define IDC_WAVE_CHOOSE                 1041
+#define IDC_PIXEL_CHOOSE                1042
+#define IDC_RESOLUTION_GET              1043
+#define IDC_COMBO3                      1044
+#define IDC_STATIC_STATUS               1045
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        137
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1046
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
